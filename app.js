3d055878af761e5f11237e0badaa87f76e84e304
Lauzaki <L@L.local>
2021-08-17 18:28:57 +0100

    add multer

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const path = require('path');
-const express = require('express')
-const app = express()
+
 const multer = require("multer")
 const fs = require('fs')
-const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg');
-const ffmpeg = createFFmpeg({ log: true });
+const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg')
+const ffmpeg = createFFmpeg({ log: true })
 
-const port = 3000
-
+//Handling of file upload to the server-------------------------------------------------------
 //multer setup
 const fileStorage = multer.diskStorage({
-  destination: 'temp/uploads',
+  destination: 'temp',
   filename: (req, file, cb) => {
-    let fileName = file.fieldname + '_' + path.extname(file.originalname);
+    const id = req.headers.socketid;
+    let fileName = 'upload_' + id + path.extname(file.originalname);
     cb(null, fileName)
   }
 });
@@ -21,26 +20,94 @@ const fileStorage = multer.diskStorage({
 const upload = multer({
   storage: fileStorage,
   limits: {
-    fileSize: 1000000000 // 1000000 Bytes = 1 MB
+    fileSize: 100000000, // 1000000 Bytes = 1 MB
+    fileFilter(req, file, cb) {
+      if (!file.originalname.match(/\.(flac|ogg|wav)$/)) {
+        return cb(new Error('File is not a flac, ogg or wav format'))
+      }
+      cb(undefined, true)
+    }
   }
 });
 
+//Compression of the audio file--------------------------------------------------------------
 async function compressFile(req, res) {
-  await ffmpeg.load();
-  ffmpeg.FS('writeFile', 'file.wav', await fetchFile('./temp/uploads/music_.wav'));
-  await ffmpeg.run('-i', 'file.wav', 'test.mp3');
-  await fs.promises.writeFile('./temp/compressed/test.mp3', ffmpeg.FS('readFile', 'test.mp3'));
-  process.exit(0);
+  const id = req.headers.socketid;
+  if (!ffmpeg.isLoaded()) await ffmpeg.load();
+  ffmpeg.FS('writeFile', 'file.wav', await fetchFile('./temp/upload_' + id +'.wav'));
+  await ffmpeg.run('-i', 'file.wav', 'temp.mp3');
+  await fs.promises.writeFile('./temp/done__'+ id +'.mp3', ffmpeg.FS('readFile', 'temp.mp3'));
+  io.to(id).emit('event','File_Ready');
+  fs.unlink(__dirname + '/temp/upload_'+id+'.wav', unlinkCb);
 }
 
-app.use(express.static('public'))
+
+// Handling of the file download to the client once it has been compressed--------------------
+
+//middleware for /download route 
+function downloadFile(req, res) {
+  //user clicked the download button
+  const id = req.headers.socketid;
+  const file = path.join(__dirname, '/temp/done__' + id + '.mp3')
+  res.download(file, 'compressedAudio.mp3', function(err){
+
+    if (err) {
+      console.log(err);
+    } else {
+      deleteDoneFiles(id);
+    }
+  
+  });
+}
+
+//delete done files
+
+function deleteDoneFiles (id){
+  fs.unlink(__dirname + '/temp/done__'+id+'.mp3', unlinkCb);
+
+  //tell the browser we are done
+  io.to(id).emit('event','Done');
+}
+
+function unlinkCb(e){
+  //console.log(e);
+}
+
+// Server-------------------------------------------------------------------------------
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+//Express
+app.use(cors())
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-app.post('/upload' , upload.single('music'), compressFile);
+//Socket.io
+io.on('connection', (socket) => {
+  console.log('a user connected   '+ socket.id);
+  let socketId = socket.id;
+  socket.emit('socketId', socketId);
+  socket.on('disconnect', () => {
+    console.log('disconnect  '+ socketId);
+    deleteDoneFiles(socketId);
+  });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.post('/upload' , upload.single('audioFile'), compressFile);
+app.get('/download', downloadFile);
+
+server.listen(3004, () => {
+  console.log(`Example app listening at http://localhost:3004`)
 })
+
--- public/script.js
+++ public/script.js
@@ -1,15 +1,89 @@
-const form = document.getElementById("form");
-form.addEventListener("submit", submitForm);
 
+const form = document.getElementById("form");
+const downloadBtn = document.getElementById("downloadBtn");
+const processing = document.getElementById("processing");
+const socketId = document.getElementById("socketId");
+const menu = document.getElementById("menu");
+const againBtn = document.getElementById("againBtn");
+
+
+form.addEventListener("submit", submitForm);
+downloadBtn.addEventListener("click", download);
+againBtn.addEventListener("click", refleshPage);
+
+// Handling of the session
+
+let sessionId;
+
+var socket = io.connect();
+socket.onAny((event, ...args) => {
+    switch (args[0]){
+        case 'File_Ready':
+            displayDownload();
+        break;
+        case 'Done':
+            menu.style.display = "block";
+        break;
+        default:
+            sessionId = args[0];
+            socketId.value = sessionId;
+            console.log(sessionId)
+  }});
+
+
+
+//display download btn
+function displayDownload (){
+    downloadBtn.style.display = 'block';
+    processing.style.display = "none";
+}
+
+//download compressed file
+function download() {
+    downloadBtn.style.display = 'none';
+    
+     axios({
+        method: 'get',
+        url: 'download',
+        responseType: 'blob',
+        headers: {
+            'SocketId': sessionId
+          }})
+      .then( res => {
+        const url = URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'compressed_audio.mp3');
+        document.body.appendChild(link);
+        link.click();
+      });
+     
+  }
+  
+  //reflesh page on AgainBtn click
+  
+  function refleshPage(){
+      location.reload();
+  
+  }
+
+// Upload of the audio file
+
+form.addEventListener("submit", submitForm);
 
 function submitForm(e) {
     e.preventDefault();
-    const music = document.getElementById("music");
+    form.style.display = "none";
+    processing.style.display = "block";
+    const audio = document.getElementById("audioFile");
     const formData = new FormData();
-    formData.append("music", music.files[0], 'music.wav');
+    formData.append("audioFile", audio.files[0], 'audio.wav');
     fetch("/upload", {
         method: 'post',
-        body: formData
+        body: formData,
+        headers: {
+            'SocketId': sessionId
+          },
     })
         .catch((err) => ("Error occured", err));
 }
